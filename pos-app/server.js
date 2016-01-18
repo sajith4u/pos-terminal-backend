705905fb030f5d38a3d3817a8b9d78d8bc49ca7a
sajith4u <sajith.vijesekara@gmail.com>
2016-01-18 17:57:46 +0530

    Add Express js pos backend integration updated

--- pos-app/server.js
+++ pos-app/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var database = require('sequelize');
 var app = express();
 var bodyParser = require('body-parser');
 
@@ -6,7 +7,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 9090;
+var port = process.env.PORT || 9091;
 
 var router = express.Router();
 
@@ -26,13 +27,116 @@ router.get('/products', function (req, res) {
         {
             product: [
                 {
-                    id: '00001', name: 'Burger1', price: "Rs 200/-"
+                    id: '00001',
+                    name: 'Burger1',
+                    price: "Rs 200/-"
                 },
                 {id: '00002', name: 'Burger2', price: "Rs 300/-"},
                 {id: '00003', name: 'Burger3', price: "Rs 400/-"},
                 {id: '00004', name: 'Burger4', price: "Rs 500/-"},
                 {id: '00005', name: 'Burger5', price: "Rs 600/-"},
                 {id: '00006', name: 'Burger6', price: "Rs 700/-"}
+            ]
+        }
+    );
+});
+
+router.get('/users', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.json(
+        {
+            users: [
+                {
+                    id: '89611253',
+                    firstName: "John",
+                    name: 'Winters',
+                    birthDate: "1991/01/27",
+                    points: 200,
+                    joinedDate: "2012/12/02",
+                    history: {
+                        items: [{
+                            itemName: "Chicken Burger",
+                            datePurchased: "2015/12/05",
+                            price: "Rs 250/-"
+                        }]
+                    }
+                },
+                {
+                    id: '89611253',
+                    firstName: "John",
+                    name: 'Winters',
+                    birthDate: "1991/01/27",
+                    points: 200,
+                    joinedDate: "2012/12/02",
+                    history: {
+                        items: [{
+                            itemName: "Chicken Burger",
+                            datePurchased: "2015/12/05",
+                            price: "Rs 250/-"
+                        }]
+                    }
+                },
+                {
+                    id: '89611253',
+                    firstName: "John",
+                    name: 'Winters',
+                    birthDate: "1991/01/27",
+                    points: 200,
+                    joinedDate: "2012/12/02",
+                    history: {
+                        items: [{
+                            itemName: "Chicken Burger",
+                            datePurchased: "2015/12/05",
+                            price: "Rs 250/-"
+                        }]
+                    }
+                },
+                {
+                    id: '89611253',
+                    firstName: "John",
+                    name: 'Winters',
+                    birthDate: "1991/01/27",
+                    points: 200,
+                    joinedDate: "2012/12/02",
+                    history: {
+                        items: [{
+                            itemName: "Chicken Burger",
+                            datePurchased: "2015/12/05",
+                            price: "Rs 250/-"
+                        }]
+                    }
+                },
+                {
+                    id: '89611253',
+                    firstName: "John",
+                    name: 'Winters',
+                    birthDate: "1991/01/27",
+                    points: 200,
+                    joinedDate: "2012/12/02",
+                    history: {
+                        items: [{
+                            itemName: "Chicken Burger",
+                            datePurchased: "2015/12/05",
+                            price: "Rs 250/-"
+                        }]
+                    }
+                },
+                {
+                    id: '89611253',
+                    firstName: "John",
+                    name: 'Winters',
+                    birthDate: "1991/01/27",
+                    points: 200,
+                    joinedDate: "2012/12/02",
+                    history: {
+                        items: [{
+                            itemName: "Chicken Burger",
+                            datePurchased: "2015/12/05",
+                            price: "Rs 250/-"
+                        }]
+                    }
+                }
             ]
         }
     );
